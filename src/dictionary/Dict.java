e7d1997cb76019b7cb8d621a6a87114ff2031ab3
fkxcole <fkxcole@gmail.com>
2014-12-01 11:09:15 +0800

    add: socket communication

--- src/dictionary/Dict.java
+++
@@ -1,9 +0,0 @@
-package dictionary;
-
-public class Dict {
-	public static void main(String[] args) {
-		//entry
-		dictServer myServer=new dictServer();
-		
-	}
-}
--- src/dictionary/dictServer.java
+++ src/dictionary/dictServer.java
@@ -1,10 +1,60 @@
 package dictionary;
 
 import java.util.*;
+
 import commonType.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.*;
 
 public class dictServer {
+	//Internet communication
+	static final int portNo = constants.PORT_NO;
+	protected class ServerThread extends Thread{
+		private Socket clientSocket;
+		private BufferedReader sin;
+		private PrintWriter sout;
+		
+		public ServerThread(){
+		}
+		public ServerThread(Socket s) throws IOException{
+			clientSocket=s;
+			sin= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			sout= new PrintWriter(new BufferedWriter(
+									new OutputStreamWriter(clientSocket.getOutputStream())),true);
+			start();
+		}
+		public void run(){
+			//处理客户端的请求
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		dictServer myServer= new dictServer();
+		ServerSocket s= new ServerSocket(portNo);
+		System.out.println("The Server is start: " + s);
+		try{
+			for(;;){
+				//阻塞等待
+				Socket socket=s.accept();
+				System.out.println("a client joined.");
+				myServer.new ServerThread(socket);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			s.close();
+		}
+	}
+	
+	
+	
 	Trie trieTree= new Trie();
 	ArrayList<User> activeUsers=new ArrayList<User>();
 	
